970e675e5637887bd8faec567da1892e55a60d7a
Nothingboy024 <156678321+Nothingboy024@users.noreply.github.com>
2025-07-12 18:23:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,14 +6,14 @@ import os
 id_pattern = re.compile(r'^.\d+$')
 
 
-API_ID = os.environ.get("API_ID", "10811400")
-API_HASH = os.environ.get("API_HASH", "191bf5ae7a6c39771e7b13cf4ffd1279")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7871689536:AAEhj97j5TszB10j7KHkEa_UadXEiJkD8ac")
-ADMIN = int(os.environ.get("ADMIN", '6469754522'))
+API_ID = os.environ.get("API_ID", "16995961")
+API_HASH = os.environ.get("API_HASH", "8817a7d4293049593e60999359970ddd")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7635764674:AAFpVoIDVLXG7c-nMmQv1rT7nL-9rYZ9XYI")
+ADMIN = int(os.environ.get("ADMIN", '7364106679'))
 #ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
 SUNRISES_PIC= "https://envs.sh/u0Z.jpg/IMG20250701161.jpg" # Replace with your Telegraph link - Start Pic
 INFO_PIC= "https://envs.sh/u05.jpg/IMG20250701173.jpg" # Replace with your Telegraph link - Information 
-UPDATES_CHANNEL = os.getenv("UPDATES_CHANNEL", "https://t.me/Sunrises24BotUpdates") # Replace with your Updates link
-SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "https://t.me/Sunrises24BotSupport") # Replace with your Support link
+UPDATES_CHANNEL = os.getenv("UPDATES_CHANNEL", "https://t.me/Toon_Encodes_Telugu") # Replace with your Updates link
+SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "https://t.me/forcesubnothingboy") # Replace with your Support link
 WEBHOOK = bool(os.environ.get("WEBHOOK", True))
 PORT = int(os.environ.get("PORT", "8081")) #for koyeb 8080 only
